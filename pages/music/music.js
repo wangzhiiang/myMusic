5a71d3f6b42bb107e5549f17c344305bbe819670
wangzhiqiang <1071683940@qq.com>
2020-11-01 10:56:08 +0800

    update

--- pages/music/music.js
+++ pages/music/music.js
@@ -1,87 +1,83 @@
-const myAduio = wx.createInnerAudioContext()
+const myAduio = wx.createInnerAudioContext();
 
 function timeFilter(val) {
-  let minutes = Math.floor(val / 60)
-  let seconds = Math.ceil(val - minutes * 60)
+  let minutes = Math.floor(val / 60);
+  let seconds = Math.ceil(val - minutes * 60);
   if (minutes < 10) {
-    minutes = '0' + minutes
+    minutes = '0' + minutes;
   }
   if (seconds < 10) {
-    seconds = '0' + seconds
+    seconds = '0' + seconds;
   }
-  return minutes + ':' + seconds
+  return minutes + ':' + seconds;
 }
 Page({
   data: {
     isPlay: true,
     sliderVal: 0,
     currentTime: '00:00',
-    duration: '',
+    duration: ''
   },
-  onReady: function () {
+  onReady: function() {
     wx.setNavigationBarTitle({
       title: 'Right Now'
-    })
-    myAduio.autoplay = true
-    myAduio.loop = true
-    myAduio.src = 'http://isure.stream.qqmusic.qq.com/C400004KFEw81z0ysI.m4a?vkey=044C59251A40D1DDEF0AFF95B9F28B0930D2CE8849A4A16562DC45179720FF3E0D3593A25E0B5C3004EA2CD155F90E514D0DC08A00293634&guid=107379363&uin=1071683940&fromtag=66'
+    });
+    myAduio.autoplay = true;
+    myAduio.loop = true;
+    myAduio.src =
+      'https://6d75-music-69c2fc-1257474226.tcb.qcloud.la/C400004KFEw81z0ysI.m4a?sign=31c4f09e8e9692a1b80b5d14306a52a7&t=1566983134';
 
     myAduio.onPlay(() => {
-      console.log(myAduio.duration)
-    })
+      console.log(myAduio.duration);
+    });
 
     myAduio.onTimeUpdate(() => {
-      this.musicUpdate()
-    })
+      this.musicUpdate();
+    });
 
-    myAduio.onEnded(() => {
+    myAduio.onEnded(() => {});
 
-    })
-
-    myAduio.onError((res) => {
-      console.log(res.errMsg)
-      console.log(res.errCode)
-    })
+    myAduio.onError(res => {
+      console.log(res.errMsg);
+      console.log(res.errCode);
+    });
   },
   musicUpdate() {
-    let duration = timeFilter(myAduio.duration)
-    let currentTime = timeFilter(myAduio.currentTime)
-    let sliderVal = Math.floor(myAduio.currentTime / myAduio.duration * 100)
+    let duration = timeFilter(myAduio.duration);
+    let currentTime = timeFilter(myAduio.currentTime);
+    let sliderVal = Math.ceil((myAduio.currentTime / myAduio.duration) * 100);
     this.setData({
       sliderVal: sliderVal,
       currentTime: currentTime,
       duration: duration
-    })
+    });
   },
-  sliderChange: function (e) {
-    console.log(myAduio.duration)
-    let currentTime = myAduio.duration * e.detail.value / 100
+  sliderChange: function(e) {
+    let currentTime = (myAduio.duration * e.detail.value) / 100;
     myAduio.seek(currentTime, () => {
       this.setData({
-        sliderVal: e.detail.value,
-      })
-    })
+        sliderVal: e.detail.value
+      });
+    });
   },
-  playMusic: function () {
-    console.log(this)
-    let isPlay
+  playMusic: function() {
+    let isPlay;
     if (this.data.isPlay) {
-      myAduio.pause()
-      isPlay = false
+      myAduio.pause();
+      isPlay = false;
     } else {
-      myAduio.play()
-      isPlay = true
+      myAduio.play();
+      isPlay = true;
     }
     this.setData({
       isPlay: isPlay
-    })
+    });
   },
-  toggleMusic: function (type) {
-    console.log(123)
+  toggleMusic: function(type) {
     myAduio.seek(0, () => {
       this.setData({
-        sliderVal: 0,
-      })
-    })
+        sliderVal: 0
+      });
+    });
   }
-})
+});
